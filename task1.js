4e00adfe58c683c0b21597612685a3429f6ac2a3
MiguelLeonardoPineda <santiagokymm@outlook.com>
2025-08-26 15:05:29 +0000

    tarea

--- task1.js
+++ task1.js
@@ -1,5 +1,17 @@
-export class Player {
-  constructor() {
-    
+class Player {
+  constructor(name, level) {
+    this.name = name;
+    this.level = level;
   }
+
+  info() {
+    console.log(`${this.name} has reached Level ${this.level}!`);
   }
+
+  levelUp() {
+    this.level++;
+  }
+}
+
+// 👇 Esto es lo que hace que el test encuentre la clase como taskX.Player
+module.exports = { Player };
--- task3.js
+++ task3.js
@@ -1,8 +1,19 @@
-export class Player {
-  constructor() {
-  
+class Player {
+  constructor(name, level) {
+    this.name = name;
+    this.level = level;
   }
 
+info() {
+  console.log(`${this.name} has reached Level ${this.level}`);
+     }
 
+  }
 
+  levelUp() {
+    this.level++;
+  }
 }
+
+// Exportar como objeto con la clave Player (porque el test usa task4.Player)
+module.exports = { Player };
